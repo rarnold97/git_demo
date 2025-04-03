fb62538244bb23aa670f8707ddce24369781c131
Ryan Arnold <rarnold@Ryans-Mac-mini.local>
2025-04-02 21:36:53 -0400

    manager's commit.

--- feature_a.py
+++ feature_a.py
@@ -3,3 +3,11 @@ import time
 import logging
 from dataclasses import dataclass
 from pathlib import Path
+
+
+def main():
+   print("I am the manager, do as I say!")
+
+
+if __name__ == "__main__":
+   main()
