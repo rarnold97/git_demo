a0013e3325b7fd6447bcd2d9a229040d9520d64e
Ryan Arnold <rarnold@Ryans-Mac-mini.local>
2025-04-02 21:39:21 -0400

    manager said you need this commit.

--- feature_a.py
+++ feature_a.py
@@ -7,6 +7,7 @@ from pathlib import Path
 
 def main():
    print("I am the manager, do as I say!")
+   print("I am the manager and I am saying this line of code is something you need!")
 
 
 if __name__ == "__main__":
